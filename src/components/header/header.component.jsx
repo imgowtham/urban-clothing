c665e16e042000df1bd0de5e1e901cbad651dbc8
Gowtham <gowtham.079@gmail.com>
2019-12-21 20:08:14 +0530

    Show Cart Dropdown Dynamically

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,8 +5,10 @@ import {auth} from '../../firebase/firebase.utils';
 import {ReactComponent as Logo} from '../../assets/logo.svg';
 
 import './header.styles.scss'
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className="logo-container" to="/">
             <Logo className='logo'/>
@@ -24,11 +26,20 @@ const Header = ({currentUser}) => (
                     SIGN IN
                 </Link>
 }
-{console.log(currentUser)}
+            <CartIcon/>
         </div>
+        {hidden
+            ? null
+            : <CartDropdown/>
+}
+
     </div>
 );
 
-const mapStateToProps = (state) => ({currentUser: state.user.currentUser})
+const mapStateToProps = ({user: {
+        currentUser
+    }, cart: {
+        hidden
+    }}) => ({currentUser, hidden})
 
 export default connect(mapStateToProps)(Header)
