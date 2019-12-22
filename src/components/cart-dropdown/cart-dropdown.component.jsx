13483d4e207b6c5ab83aa9bdc5bb7f9757a278da
Gowtham <gowtham.079@gmail.com>
2019-12-22 12:27:38 +0530

    Added Cart Item and connected with Cart dropdowns and cart reducers

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-
+import {connect} from 'react-redux'
+import { Scrollbars } from 'react-custom-scrollbars';
 import CustomButton from '../custom-buttom/custom-button.component'
 
 import './cart-dropdown.styles.scss'
+import CartItem from '../cart-item/cart-item.component';
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
     <div className="cart-dropdown">
-        <div className="cart-items"/>
+        <Scrollbars autoHide autoHideTimeout={1000} autoHideDuration={200}>
+        <div className="cart-items">
+            {cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
+            }
+        </div>
+        </Scrollbars>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
-
-export default CartDropdown
+const mapStateToProps = ({cart: {
+        cartItems
+    }}) => ({cartItems})
+export default connect(mapStateToProps, null)(CartDropdown)
