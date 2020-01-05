108652e3628940db6247c206edfc89e0062192d4
Gowtham <gowtham.079@gmail.com>
2020-01-05 22:29:31 +0530

    adding collecctions overview page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,15 @@
 import React from 'react';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
-import Collections from './collection.data'
+import {Route} from 'react-router-dom'
 
-class ShopPage extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            collections: Collections
-        }
-    }
+import CollectionOverview from '../../components/collections.overview/collections.overview.component';
+import CollectionPage from '../collection/collection.component';
 
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps}/>
-                    ))
-                }
-            </div>
-        )
-    }
-}
+const ShopPage = ({match}) => (
+    
+    <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionOverview}/>
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+    </div>
+)
 
-export default ShopPage;
+export default ShopPage
