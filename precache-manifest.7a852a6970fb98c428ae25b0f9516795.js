ad7743ba06fd3fb4ad177c98f73daddcfc0540a6
Gowtham <gowtham.079@gmail.com>
2021-06-15 13:33:16 +0530

    Updates

---
+++ precache-manifest.7a852a6970fb98c428ae25b0f9516795.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e757abd94f9de5459c2fd6e7399126f3",
+    "url": "/urban-clothing/index.html"
+  },
+  {
+    "revision": "b41f591bf12d313f1372",
+    "url": "/urban-clothing/static/css/main.6da6c986.chunk.css"
+  },
+  {
+    "revision": "401f0bcb6b8ac649fa2d",
+    "url": "/urban-clothing/static/js/2.6c5a334e.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/urban-clothing/static/js/2.6c5a334e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b41f591bf12d313f1372",
+    "url": "/urban-clothing/static/js/main.0673d0ad.chunk.js"
+  },
+  {
+    "revision": "345f69f7f8d859589126",
+    "url": "/urban-clothing/static/js/runtime-main.9663b271.js"
+  },
+  {
+    "revision": "e822b152d1d67af07d99dab63fce3422",
+    "url": "/urban-clothing/static/media/logo.e822b152.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/urban-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
